fca00841fcb4751e1a5b8b306df8a53d7482d13d
burstknight <burstknight@gmail.com>
2022-06-27 11:06:05 +0800

    doc: Add Readme
    Add Readme.

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# TestMNIST
+This is my practice for deep learning. I use [MNIST dataset](http://yann.lecun.com/exdb/mnist/) to try network.
---
+++ src/myDataset.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+
+class myDataset:
+    """
+    Description:
+    ============================================
+    This class can read MNIST dataset.
+    """
+# End of class myDataset
